84cd5849453eff7cda55dcb30d4aae05b88da833
Conner Simmons <conner@ionic.io>
2022-05-24 18:23:47 -0400

    watch live link update

--- src/app/util/core.constants.ts
+++ src/app/util/core.constants.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const CoreConstants = {
   prismicID: 'YlmLZxIAACkAdajh',
-  eventUrl: 'https://ionic.io/events',
+  eventUrl: 'https://ionic.io/ioniconf',
   privacyPolicyUrl: 'https://ionic.io/privacy',
   minutesBefore: 5,
 };
